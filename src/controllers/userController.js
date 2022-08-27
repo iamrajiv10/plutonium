336f9876c3454ce2627d63cc33280575ae7c2765
iamrajiv10 <iamrajiv10@gmail.com>
2022-08-27 14:44:44 +0530

    demo

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,51 @@
+const userModel = require("../models/userModel")
 const UserModel= require("../models/userModel")
 
 
+/*const createOrder = async function (req, res) {
+    let header = req.headers["isfreeappuser"]
+    
+    if(!header){
+        res.send({msg: "header is not present"})
+    }
+    //product and user validation
 
+    let product = await productModel.findById(productId)
+    if (!product){
+        res.send({msg: "product doesn't exist"})
+    }
+
+    let user = await userModel.findById(userId)
+    if (!user){
+        res.send({msg: "userdoesn't exist"})
+    }
+    let headerValue = false 
+    if(header === 'true'){
+        headerValue = true
+    }
+    let oreder={
+         isFreeAppUser: headerValue,
+         productId:productId,
+         userId: userId
+    }
+    if(headerValue) {
+        oreder.amount = 0
+        let orderCreated = await orderModel.create(order)
+        res.send({msg:"order created"})
+    }else{
+        if(user.balance >= product.price){
+            user.balance = user.balance - product.price
+            let updateUser = await user.save()
+            order.amount = product.price
+            let orderCreated = await orderModel.create(order)
+            res.send({msg:"order created"})
+        } else{
+            res.send({msg: "User has insufficient balance"})
+        }
+    }
+
+    res.send({msg: "done"})
+}
 
 const basicCode= async function(req, res, next) {
     let tokenDataInHeaders= req.headers.token
@@ -35,7 +79,7 @@ const createUser= async function (req, res) {
     //Set a header in response
     res.header('year','2022')
     res.send({msg: "Hi"})
-}
+}*/
 
 const getUsersData= async function (req, res) {
     let allUsers= await UserModel.find()
@@ -45,3 +89,4 @@ const getUsersData= async function (req, res) {
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
 module.exports.basicCode= basicCode
+module.exports.createOrder= createOrder
