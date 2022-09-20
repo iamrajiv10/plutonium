1ac78850b4cc7e9b651e98427e6a8c6638486d7b
iamrajiv10 <iamrajiv10@gmail.com>
2022-09-20 13:55:39 +0530

    folders

---
+++ src/routes/route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const bookController = require("../Controller/bookController");
+const userController = require("../Controller/userController");
+
+
+module.exports = router;
